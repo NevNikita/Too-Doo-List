630c3ed9a11aab8fcc57c8073e03daa15d4725fc
Nikita Newodov <nevodovnik@gmail.com>
2020-07-10 22:03:58 +0300

    ...

---
+++ Too-Doo List/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Too_Doo_List.VeiwModels;
+using Too_Doo_List.Views;
+
+namespace Too_Doo_List
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateTaskView_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new CreateTaskModel();
+        }
+
+        private void ViewTask_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new ViewTaskModel();
+        }
+    }
+}
---
+++ Too-Doo List/Views/CreateTask.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Too_Doo_List.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateTask.xaml
+    /// </summary>
+    public partial class CreateTask : UserControl
+    {
+        public CreateTask()
+        {
+            InitializeComponent();
+        }
+    }
+}
